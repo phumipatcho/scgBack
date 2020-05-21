08a62f1fbe7ecb77f23f320ad29ecc51b4f07494
sudlor_gang <bassudlor@gmail.com>
2020-05-21 10:08:15 +0700

    add vue to project

---
+++ vue/real-world-vue/src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home contrainer">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <button class="btn btn-primary">click</button>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld.vue";
+
+export default {
+  name: "Home",
+  components: {
+    HelloWorld
+  }
+};
+</script>
